ee98e43c4c42d3a226b3ed79ca9824a96fa12d9c
Freddy Andersen <freddy@zerolimitstech.com>
2010-11-30 13:01:31 -0800

    moving module back to 3.1.2

--- config/environment.rb
+++ config/environment.rb
@@ -7,14 +7,6 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # moved to bcms 3.2
-  config.gem 'browsercms', :version => '3.2.0'
-
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
+  config.gem 'browsercms', :version => '3.1.2'
   config.time_zone = 'UTC'
-
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :en
 end
